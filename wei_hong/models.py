2e27463b33fc39ff37d0d8a6e934f19b8b425985
alexzeng2014 <mianhu@gmail.com>
2026-06-01 20:07:08 +0800

    Add wei_hong/models.py

---
+++ wei_hong/models.py
@@ -0,0 +1,19 @@
+﻿from dataclasses import dataclass, field
+from typing import Optional
+
+
+@dataclass
+class ArticleItem:
+    """统一搜索结果模型"""
+    platform: str              # "wechat" | "xiaohongshu"
+    title: str
+    url: str
+    abstract: str = ""
+    author: str = ""
+    author_avatar: str = ""
+    publish_time: str = ""
+    cover_image: str = ""
+    source_name: str = ""      # 公众号名称 / 小红书作者昵称
+    source_id: str = ""        # 微信号 / 小红书用户ID
+    note_id: str = ""          # 小红书笔记ID
+    extra: dict = field(default_factory=dict)
